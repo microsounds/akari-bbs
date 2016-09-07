f986c36e1565ddf1e5e475f7bd9b0a0e5b6b0e8c
microsounds <microsounds@users.noreply.github.com>
2016-09-07 10:39:47 -0700

    Post quoting behavior now inserts at cursor position

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 function quote(text)
 {
-	/* click to quote posts */
-	var elem = document.getElementById("pBox");
-	elem.value += ">>" + text + "\n";
+	/* insert quoted post into textarea */
+	var el = document.getElementById("pBox");
+	var before = el.value.slice(0, el.selectionStart);
+	var after = el.value.slice(el.selectionStart);
+	var insert = ">>" + text + "\n";
+	var caretPos = before.length + insert.length;
+	el.value = before + insert + after;
+	el.setSelectionRange(caretPos, caretPos);
+	el.focus();
 }
